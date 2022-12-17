f2df50eb38bc0e25c76fba626991c45b4df86a7b
Sreyaea <sreya.ea@gmail.com>
2022-12-17 14:43:35 +0530

    with api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow'
+const BASE_URL='https://api.exchangerate.host/latest'
 
 function App() {
+  useEffect(() =>{
+    fetch(BASE_URL)
+     .then(res =>res.json())
+     .then (data => console.log(data))
+   },[])
   return (
     <>
       <h1>Convert</h1>
       <CurrencyRow />
-      <div>=</div>
+      <div className="equals">=</div>
       <CurrencyRow />
     </>
   );
